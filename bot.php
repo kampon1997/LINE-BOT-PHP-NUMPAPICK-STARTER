368e0abb42885de5ed89f3bb056cfe5ed80a3650
kampon1997 <53436709+kampon1997@users.noreply.github.com>
2019-09-11 20:32:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,29 +14,5 @@ if (!is_null($events['ESP'])) {
 		
 	echo "OK";
 	}
-if (!is_null($events['events'])) {
-	echo "line bot";
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-
-			$Topic = "NodeMCU1" ;
-			getMqttfromlineMsg($Topic,$text);
-			   
-			
-		}
-	}
-}
-$Topic = "NodeMCU1" ;
-$text = "Test";
-getMqttfromlineMsg($Topic,$text);
-echo "OK3";
 ?>
 
